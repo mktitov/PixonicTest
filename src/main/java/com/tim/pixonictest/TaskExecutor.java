22a6f572a48f9fe91eba7d2f37373cd313945d21
tim <tim@tim-nb>
2016-07-14 02:38:41 +0300

    Исправлена ошибка выполнения задач не в корректной последовательности

--- src/main/java/com/tim/pixonictest/TaskExecutor.java
+++ src/main/java/com/tim/pixonictest/TaskExecutor.java
@@ -7,28 +7,115 @@ package com.tim.pixonictest;
 
 import java.util.Date;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  *
  * @author Mikhail Titov
  */
 public class TaskExecutor {
-    public final static int THREADS_COUNT = Runtime.getRuntime().availableProcessors(); //для полной гарантии очередности выполнения задач необходимо выставить в единицу :)
+    //для полной гарантии очередности выполнения задач необходимо выставить в единицу
+    public final static int THREADS_COUNT = Runtime.getRuntime().availableProcessors(); 
+    public final static long MAX_POLL_INTERVAL = 1000L;
+    //что бы показать, что и после перехода в отрицательное множество значений все будет работать нормально
+    private final static AtomicLong SEQUENCE = new AtomicLong(Long.MAX_VALUE-10); 
     
-    private final ScheduledThreadPoolExecutor executor;
+    private final ExecutorService tasksExecutor;
+    private final ExecutorService scheduleTaskExecutor;
+    private final DelayQueue<DelayedTask> delayedTasks;
+    private final AtomicBoolean stopped;
 
     public TaskExecutor() {
-        this.executor = new ScheduledThreadPoolExecutor(THREADS_COUNT);
+        this(THREADS_COUNT);
     }
     
     public TaskExecutor(int threadsCount) {
-        this.executor = new ScheduledThreadPoolExecutor(THREADS_COUNT);        
+        this.tasksExecutor = Executors.newWorkStealingPool(threadsCount);
+        this.delayedTasks = new DelayQueue<>();
+        this.stopped = new AtomicBoolean();
+        this.scheduleTaskExecutor = Executors.newSingleThreadExecutor();        
+        this.scheduleTaskExecutor.execute(new ScheduleWorker());
     }
+    
+    public <V> Future<V> execute(Date dateTime, Callable<V> task) {
+        FutureTask<V> future = new FutureTask<>(task);
+        delayedTasks.offer(new DelayedTask(dateTime.getTime(), future, task));
+        return future;
+    }
+    
+    public void stop() {
+        if (stopped.compareAndSet(false, true)) {
+            scheduleTaskExecutor.shutdown();
+            tasksExecutor.shutdown();            
+        }
+    }
+    
+    private class ScheduleWorker implements Runnable {
+        
+        @Override 
+        public void run() {
+            while (!stopped.get()) {
+                try {
+                    DelayedTask task = delayedTasks.poll(MAX_POLL_INTERVAL, TimeUnit.MILLISECONDS);
+                    if (task!=null) 
+                        tasksExecutor.execute(task.task);                        
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();                    
+                }
+            }
+        }        
+    }
+    
+    private class DelayedTask<V> implements Delayed {
+        
+        private final long startTime;
+        private final FutureTask<V> task;
+        private final Callable<V> originalTask;
+        private final long seqNumber = SEQUENCE.getAndIncrement();
 
-    public <V> ScheduledFuture<V> execute(Date dateTime, Callable<V> task) {
-        return executor.schedule(task, dateTime.getTime() - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        public DelayedTask(long startTime, FutureTask<V> task, Callable<V> originalTask) {
+            this.startTime = startTime;
+            this.task = task;
+            this.originalTask = originalTask;
+        }
+        
+        @Override
+        public long getDelay(TimeUnit tu) {
+            return tu.convert(startTime - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed dt) {
+            if (dt==this)
+                return 0;
+            DelayedTask t = (DelayedTask) dt;
+            if (seqNumber==t.seqNumber)
+                return 0;
+            else
+                return seqNumber - t.seqNumber < 0? -1 : 1;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj==this)
+                return true;
+            else if (!(obj instanceof DelayedTask))
+                return false;
+            else 
+                return this.startTime==((DelayedTask)obj).startTime;
+        }
+
+        @Override
+        public String toString() {
+            return originalTask.toString(); 
+        }
     }
 }
--- src/test/java/com/tim/pixonictest/TaskExecutorTest.java
+++ src/test/java/com/tim/pixonictest/TaskExecutorTest.java
@@ -1,6 +1,7 @@
 package com.tim.pixonictest;
 
 import java.util.Date;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.Callable;
@@ -23,7 +24,17 @@ public class TaskExecutorTest {
         TaskExecutor executor = new TaskExecutor(1);
         execute(executor, 1000);
         Thread.sleep(1100L);        
-        checkQueue(1, 100);
+        checkQueue(1, 100, false);
+    }
+    
+    @Test
+    public void orderedExecutionTest() throws Exception {
+        long startTime = System.currentTimeMillis() + 2000;        
+        TaskExecutor executor = new TaskExecutor(1);
+        for (int i=0; i<100; i++)
+            execute(executor, startTime, i);
+        Thread.sleep(3000L);
+        checkQueue(100, 100, true);
     }
     
     @Test
@@ -32,7 +43,7 @@ public class TaskExecutorTest {
         execute(executor, 1000);
         execute(executor, 500);
         Thread.sleep(1100L);        
-        checkQueue(2, 100);        
+        checkQueue(2, 100, false);        
     }
     
     @Test
@@ -42,7 +53,7 @@ public class TaskExecutorTest {
         execute(executor, -1000);
         execute(executor, 500);
         Thread.sleep(1000L);        
-        checkQueue(3, 100);        
+        checkQueue(3, 100, false);        
     }
     
     @Test
@@ -54,20 +65,27 @@ public class TaskExecutorTest {
             execute(executor, rnd.nextInt(STRESS_TEST_EXECUTION_TIME));
         }
         Thread.sleep(STRESS_TEST_EXECUTION_TIME+1000L);
-        checkQueue(STRESS_TEST_TASKS_COUNT, STRESS_TEST_EXECUTION_TIME);
+        checkQueue(STRESS_TEST_TASKS_COUNT, STRESS_TEST_EXECUTION_TIME, false);
     }
     
-    private void checkQueue(int expectedSize, long maxDelay) {
+    
+    private void checkQueue(int expectedSize, long maxDelay, boolean checkOrder) {
         int size = 0;
         for (Task task: startedTasks) {
-            size++;            
             assertFalse(task.execTime < task.planedExecTime.getTime()-1 || task.getExecutionTimeDelta() > maxDelay);
+            if (checkOrder)
+                assertEquals(size, task.orderNum);
+            size++;            
         }
         assertEquals(expectedSize, size);
     }
     
     private void execute(TaskExecutor executor, long delay) {
-        Task task = new Task(System.currentTimeMillis()+delay);
+        execute(executor, delay, 0);
+    }
+    
+    private void execute(TaskExecutor executor, long startTime, int orderNum) {
+        Task task = new Task(startTime, orderNum);
         executor.execute(task.getPlanedExecTime(), task);        
     }
     
@@ -75,10 +93,16 @@ public class TaskExecutorTest {
         private final Date planedExecTime;
         private final long submitTime;
         private volatile long execTime;
+        private final int orderNum;
 
-        public Task(long execTime) {            
+        public Task(long execTime) {
+            this(execTime, 0);
+        }
+
+        public Task(long execTime, int orderNum) {            
             this.planedExecTime = new Date(execTime);
             this.submitTime = System.currentTimeMillis();
+            this.orderNum = orderNum;
         }
         
         private boolean isOverdued() {
@@ -101,7 +125,13 @@ public class TaskExecutorTest {
         public Object call() throws Exception {
             execTime = System.currentTimeMillis();
             startedTasks.offer(this);
+//            System.out.println("Executig task");
             return null;
+        }
+
+        @Override
+        public String toString() {
+            return "Order num: "+orderNum; 
         }        
-    }
+    }    
 }
